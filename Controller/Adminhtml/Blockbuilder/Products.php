a03c8856b4fb712f518c8cc558d439a2b300790a
landofcoder <landofcoder@gmail.com>
2021-07-23 09:50:30 +0700

    init products grid

---
+++ Controller/Adminhtml/Blockbuilder/Products.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Venustheme
+ * 
+ * NOTICE OF LICENSE
+ * 
+ * This source file is subject to the Venustheme.com license that is
+ * available through the world-wide-web at this URL:
+ * http://www.venustheme.com/license-agreement.html
+ * 
+ * DISCLAIMER
+ * 
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ * 
+ * @category   Venustheme
+ * @package    Ves_PageBuilder
+ * @copyright  Copyright (c) 2014 Venustheme (http://www.venustheme.com/)
+ * @license    http://www.venustheme.com/LICENSE-1.0.html
+ */
+namespace Ves\PageBuilder\Controller\Adminhtml\Blockbuilder;
+
+class Products extends \Magento\Catalog\Controller\Adminhtml\Product
+{
+    /**
+     * @var \Magento\Framework\View\Result\LayoutFactory
+     */
+    protected $resultLayoutFactory;
+
+    /**
+     * @var \Magento\Framework\Registry
+     */
+    protected $coreRegistry;
+
+    /**
+     * @var \Ves\PageBuilder\Model\BlockFactory
+     */
+    protected $blockFactory;
+
+    /**
+     * @param \Magento\Backend\App\Action\Context $context
+     * @param \Magento\Catalog\Controller\Adminhtml\Product\Builder $productBuilder
+     * @param \Magento\Framework\View\Result\LayoutFactory $resultLayoutFactory
+     * @param \Magento\Framework\Registry $registry
+     * @param \Ves\PageBuilder\Model\BlockFactory $blockFactory
+     */
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Magento\Catalog\Controller\Adminhtml\Product\Builder $productBuilder,
+        \Magento\Framework\View\Result\LayoutFactory $resultLayoutFactory,
+        \Magento\Framework\Registry $registry,
+        \Ves\PageBuilder\Model\BlockFactory $blockFactory
+    ) {
+        parent::__construct($context, $productBuilder);
+        $this->resultLayoutFactory = $resultLayoutFactory;
+        $this->coreRegistry = $registry;
+        $this->blockFactory = $blockFactory;
+    }
+
+    /**
+     * @return \Magento\Framework\View\Result\Layout
+     */
+    public function execute()
+    {
+        $id = $this->getRequest()->getparam('block_id');
+        $blockbuilder = $this->blockFactory->create()->load($id);
+        $this->coreRegistry->register("ves_pagebuilder", $blockbuilder);
+
+        $this->productBuilder->build($this->getRequest());
+        $resultLayout = $this->resultLayoutFactory->create();
+        $resultLayout->getLayout()->getBlock('blockbuilder.edit.tab.products')->setProductsRelated($this->getRequest()->getPost('products', null));
+        return $resultLayout;
+    }
+}
